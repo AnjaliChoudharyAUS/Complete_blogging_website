ca2e9a54e6280b88d395f8b74511df15f2c91c3b
Anjali Choudhary <66716436+AnjaliChoudharyAUS@users.noreply.github.com>
2022-06-27 13:35:02 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+
+import React from "react";
+import BlogList from "./BlogList";
+import usefetch from "./usefetch";
+
+
+
+const Home = () => {
+    const {data:blogs, isPending,error}=usefetch('  http://localhost:8000/blogs');
+    
+   
+    return ( 
+        <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div> Loading...</div>}
+              {blogs && < BlogList blogs={blogs} title="All Blogs!" />}
+              
+              
+    
+       </div>
+      
+     );
+}
+ 
+export default Home;
